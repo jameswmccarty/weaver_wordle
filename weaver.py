18f4da4ce1f7028af2dfd66a24384feea387add0
jameswmccarty <jameswmccarty@gmail.com>
2022-07-17 08:51:20 -0400

    Speed Improvements
    Generate words that differ by one char, instead of search dict

--- weaver.py
+++ weaver.py
@@ -7,6 +7,7 @@ start, end = "wish","star"
 word_list = set()
 with open("enable1.txt","r") as infile:
 	word_list = { line.strip() for line in infile.readlines() if len(line.strip()) == len(start) }
+lower_alpha = [ chr(_) for _ in range(97,97+26) ]
 
 # Return True if words differ by 1 letter
 def diff_count_of_one(w1,w2):
@@ -18,15 +19,26 @@ def diff_count_of_one(w1,w2):
 			return False
 	return True
 
+# Return words that are valid and only differ by one character
+def next_poss_words(word):
+	next_words = []
+	for idx,char in enumerate(word):
+		for c in lower_alpha:
+			if c != char:
+				next_word = word[0:idx]+c+word[idx+1:]
+				if next_word in word_list:
+					next_words.append(next_word)
+	return next_words
+
 q = [(start,'')]
 seen = set()
 seen.add(start)
 while len(q) > 0:
 	current, chain = q.pop(0)
 	if current == end:
-		print(chain[1:]+','+end)
+		print(chain[1:]+','+end,chain.count(","))
 		exit()
-	nexts = [ word for word in word_list if word not in seen and diff_count_of_one(word,current) ]
+	nexts = [ word for word in next_poss_words(current) if word not in seen ]
 	for next in nexts:
 		q.append((next,chain+','+current))
 		seen.add(next)
