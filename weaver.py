e199541be0a57f7990fb77b27aae9eb262ee0876
jameswmccarty <jameswmccarty@gmail.com>
2022-07-17 09:10:19 -0400

    Cleanups

--- weaver.py
+++ weaver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# for https://wordwormdormdork.com/
+# for https://wordwormdormdork.com/ ; https://weaverwordle.com/
+
 
 start, end = "stop","sign"
 
@@ -11,25 +12,24 @@ lower_alpha = [ chr(_) for _ in range(97,97+26) ]
 
 # Return True if words differ by 1 letter
 def diff_count_of_one(w1,w2):
+	if w1 == w2:	
+		return False
 	count = 0
 	for idx,char in enumerate(w1):
 		if w2[idx] != char:
 			count += 1
 		if count > 1:
 			return False
-	if count == 1:
-		return True
-	return False
+	return True
 
 # Return words that are valid and only differ by one character
 def next_poss_words(word):
 	next_words = []
 	for idx,char in enumerate(word):
-		for c in lower_alpha:
-			if c != char:
-				next_word = word[0:idx]+c+word[idx+1:]
-				if next_word in word_list:
-					next_words.append(next_word)
+		for c in [ x for x in lower_alpha if x != char]:
+			next_word = word[0:idx]+c+word[idx+1:]
+			if next_word in word_list:
+				next_words.append(next_word)
 	return next_words
 
 q = [(start,'')]
@@ -39,9 +39,8 @@ while len(q) > 0:
 	current, chain = q.pop(0)
 	if current == end:
 		print(chain[1:]+','+end,chain.count(","))
-		exit()
+		#exit()
 	nexts = [ word for word in next_poss_words(current) if word not in seen ]
 	for next in nexts:
 		q.append((next,chain+','+current))
 		seen.add(next)
-
